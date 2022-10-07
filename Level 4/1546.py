a6894c3ce69248ddc0b876585313609ee25a14e2
chlrkdgml11 <chlrkdgml11@naver.com>
2022-10-07 19:27:21 +0900

    조작한 점수의 평균 구하기

---
+++ Level 4/1546.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(input())
+
+arr = list(map(int, sys.stdin.readline().split()))
+
+M = max(arr)
+
+for i in range(N):
+    arr[i] = arr[i] / M * 100
+
+print(sum(arr) / len(arr))
