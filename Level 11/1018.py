51a8098316a14cffb66c7ef82e0cfd9392245597
chlrkdgml11 <chlrkdgml11@naver.com>
2022-10-27 02:02:09 +0900

    체스판 다시 칠하기

--- Level 11/1018.py
+++ Level 11/1018.py
@@ -1,4 +1,3 @@
-from re import L
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
@@ -11,15 +10,22 @@ for _ in range(N):
 board_W = ['WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW']
 board_B = ['BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB']
 
-W = 'WBWBWBWB'
-B = 'BWBWBWBW'
+temp = 999
 
 for i in range(N-7):
     for j in range(M-7):
-        if(arr[i][j] == 'W'):
-            
+        diff_1 = 0
+        diff_2 = 0
 
+        for a in range(8):
+            for b in range(8):
+                if(arr[i+a][j+b] != board_B[a][b]):
+                    diff_1 += 1
 
+                if(arr[i+a][j+b] != board_W[a][b]):
+                    diff_2 += 1
 
-
-
+        if(min(diff_1, diff_2) <= temp):
+            temp = min(diff_1, diff_2)
+        
+print(temp)
--- practice.py
+++ practice.py
@@ -1,17 +1,4 @@
-import sys
+a = 1
+b = 2
 
-W = 'WBWBWBWB'
-B = 'BWBWBWBW'
-
-board_01 = ['WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW']
-board_02 = ['BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB']
-
-N, M = map(int, sys.stdin.readline().split())
-
-arr = []
-
-for _ in range(N):
-    arr.append(sys.stdin.readline().strip())
-print('')
-print(arr[0][:8] == W)
-
+print(min(a, b))
