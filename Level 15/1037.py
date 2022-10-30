84e8f6c4b8e4263ab2943dedd8f1832fcc193aa7
chlrkdgml11 <chlrkdgml11@naver.com>
2022-10-30 20:29:45 +0900

    정수의 약수 개수 및 약수약수들이 주어질때 정수 구하기

---
+++ Level 15/1037.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = list(map(int, sys.stdin.readline().split()))
+
+arr.sort()
+
+if(len(arr) % 2 == 1):
+    print(arr[len(arr) // 2] ** 2)
+else:
+    print(arr[0] * arr[-1])
